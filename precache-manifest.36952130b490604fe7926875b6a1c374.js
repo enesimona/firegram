175ea108a740bfb9ab6b6e4d1539c33495900ef4
Simona Ene <enesimonam@gmail.com>
2020-11-21 11:35:51 +0200

    Updates

---
+++ precache-manifest.36952130b490604fe7926875b6a1c374.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc44d9fc9b22514e56f37bec4f04ec72",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "893a426e4d3dfd0a1ac5",
+    "url": "/firegram/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "4053384adebba33dd50b",
+    "url": "/firegram/static/css/main.021a9567.chunk.css"
+  },
+  {
+    "revision": "893a426e4d3dfd0a1ac5",
+    "url": "/firegram/static/js/2.3fe7d4b7.chunk.js"
+  },
+  {
+    "revision": "151ca73e4410028c446763ebee94bd90",
+    "url": "/firegram/static/js/2.3fe7d4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4053384adebba33dd50b",
+    "url": "/firegram/static/js/main.efe2c80c.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
